687f3b3816fcc39885e98de947fd3922fa652693
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-11-28 23:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
 
-function daysOfAYear(year) 
-	if(year % 400 == 0) return 366;
-	else if(year % 4 == 0 && year % 100 != 0) return 366;
-	else return 365;
+function daysOfAYear(year) {
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+        return 366; // Leap year
+    } else {
+        return 365; // Non-leap year
+    }
 }
